6f54789f7b8a7640a1251cda08ac7d329dba1fc7
Xander Gupton <xander.gupton@gmail.com>
2025-11-30 23:04:50 -0700

    added prison data

--- app/src/main/java/svi_prison_analysis/States/Illinois.java
+++ app/src/main/java/svi_prison_analysis/States/Illinois.java
@@ -1,17 +1,17 @@
-package svi_prison_analysis.States;
-import svi_prison_analysis.State;
+// package svi_prison_analysis.States;
+// import svi_prison_analysis.State;
 
-import org.apache.spark.sql.*;
+// import org.apache.spark.sql.*;
 
 
-public class Illinois extends State{
-    public Illinois (SparkSession spark){
-        super(spark);
-    }
+// public class Illinois extends State{
+//     public Illinois (SparkSession spark, String name){
+//         super(spark);
+//     }
 
-    @Override
-    protected String getStateName() {
-        return "Illinois";
-    }
+//     // @Override
+//     // protected String getStateName() {
+//     //     return "Illinois";
+//     // }
 
-}
+// }
